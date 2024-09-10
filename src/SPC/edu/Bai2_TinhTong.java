8b94296827d1bd746cab855a551c1c085c8be01d
NghiaPss <SPC@30-LAB402>
2024-09-10 20:16:12 +0700

    Thêm bài hai chương trình tính tổng hai số

---
+++ src/SPC/edu/Bai2_TinhTong.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SPC.edu;
+
+/**
+ *
+ * @author SPC
+ */
+public class Bai2_TinhTong {
+    public static void main(String[] args) {
+        System.out.println("Chuong trinh tinh tong hai so");
+        int a = 10, b = 10;
+        int tong = a + b;
+        System.out.println(a + " + " + b + " = " + tong);
+    }
+}
