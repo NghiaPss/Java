6bb3538716b3bb2d8fff376e4a2d235dbb18a6f6
NghiaPss <buinghia2015@gmail.com>
2024-10-15 20:27:12 +0700

    Bai10_Tinhtiendien

---
+++ src/SPC/edu/Bai10_Tinhtiendien.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SPC.edu;
+import java.util.Scanner;
+/**
+ *
+ * @author SPC
+ */
+public class Bai10_Tinhtiendien {
+    public static void main(String args){
+        int[] arrGia = {1893, 1956, 2271, 2860, 3197, 3302};
+        int [] arrBac = {0, 50, 100, 200, 300, 400};
+        System.out.println("Nhap chi so cu");
+        var sc = new Scanner(System.in);
+        int oldNum = sc.nextInt();
+        System.out.println("Nhap chi so moi");
+        int newNum = sc.nextInt();
+        int passNum = newNum - oldNum;
+       
+                
+            }
+        }
+    
